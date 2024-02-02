bf2a19b6b59bf0cbfd398f1bccef0a228ef8fce1
shashankshet <shashankshetk@gmail.com>
2024-02-02 21:15:21 +0530

    adde login logic

--- demo/src/main/java/com/example/demo/controller/Contoller.java
+++ demo/src/main/java/com/example/demo/controller/Contoller.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.LoginRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -13,5 +19,28 @@ public class Contoller {
     public String health()
     {
         return "healthy";
+    }
+
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest loginRequest) {
+        String hardcodedUsername = "demoUser";
+        String hardcodedPassword = "demoPassword";
+
+        if (loginRequest.getUsername().equals(hardcodedUsername) && loginRequest.getPassword().equals(hardcodedPassword)) {
+            String token = generateToken();
+
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+
+            return ResponseEntity.ok(response);
+        } else {
+            Map<String, String> response = new HashMap<>();
+            response.put("error", "Invalid credentials");
+
+            return ResponseEntity.status(401).body(response);
+        }
+    }
+
+    private String generateToken() {
+        return "your_static_token";
     }
 }
