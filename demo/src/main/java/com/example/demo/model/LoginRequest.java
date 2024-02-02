bb52583a74711dc401a91c4acca95eafe5dfb072
Parth P Shah <p.parthshah99@gmail.com>
2024-02-02 21:17:22 +0530

    adding getter setter

--- demo/src/main/java/com/example/demo/model/LoginRequest.java
+++ demo/src/main/java/com/example/demo/model/LoginRequest.java
@@ -6,5 +6,12 @@ import lombok.Data;
 public class LoginRequest {
     private String username;
     private String password;
+    
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
