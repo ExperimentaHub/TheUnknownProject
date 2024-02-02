a4581c42f084b66d5df42e1fdd3e53187779fd18
Parth P Shah <p.parthshah99@gmail.com>
2024-02-02 21:18:34 +0530

    Update LoginRequest.java

--- demo/src/main/java/com/example/demo/model/LoginRequest.java
+++ demo/src/main/java/com/example/demo/model/LoginRequest.java
@@ -13,5 +13,11 @@ public class LoginRequest {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
